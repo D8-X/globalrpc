683c9b9db0b3c8e5c60e3d83a0d7ce45dbee88fa
souhailaS <serbout.souhaila@gmail.com>
2026-02-18 13:58:47 +0100

    split lua script by operation and error handling

--- lua.go
+++ lua.go
@@ -1,21 +1,38 @@
 package globalrpc
 
-// Params:
-// KEYS[1] : key for curr index for the given chain
-// KEYS[2] : key for urls for the given chain
-// KEYS[3] : key for lock for the given chain
-// ARGV[1] : expiry in seconds
-const LUA_SCRIPT = `
-	-- increment the round-robin index
-	local index = redis.call("INCR", KEYS[1]) - 1
+const LUA_ACQUIRE = `
 	local urls = redis.call("LRANGE", KEYS[2], 0, -1)
+	if #urls == 0 then
+		return nil
+	end
+	local index = redis.call("INCR", KEYS[1]) - 1
 	local selectedUrl = urls[(index % #urls) + 1]
 	local lockKey = KEYS[3] .. selectedUrl
-	-- try to set the lock for this URL with a TTL
-	local lockAcquired = redis.call("SET", lockKey, 1, "NX", "EX", ARGV[1])
+	local lockAcquired = redis.call("SET", lockKey, ARGV[2], "NX", "EX", ARGV[1])
 	if lockAcquired then
 		return selectedUrl
 	else
 		return nil
 	end
+`
+
+const LUA_RELEASE = `
+	if redis.call("GET", KEYS[1]) == ARGV[1] then
+		return redis.call("DEL", KEYS[1])
+	end
+	return 0
+`
+
+const LUA_RENEW = `
+	if redis.call("GET", KEYS[1]) == ARGV[1] then
+		return redis.call("EXPIRE", KEYS[1], ARGV[2])
+	end
+	return 0
+`
+
+const LUA_NONCE_NEXT = `
+	if redis.call("EXISTS", KEYS[1]) == 0 then
+		return redis.error("nonce key not seeded: " .. KEYS[1])
+	end
+	return redis.call("INCR", KEYS[1])
 `
